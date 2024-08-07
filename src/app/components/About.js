24e0971189d87b1196cc14a14e927fb08553cc64
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-07 02:17:23 -0600

    replace placeholder content in About

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,12 +1,13 @@
 export default function About() {
     return (
       <section id="about" className="p-8">
-        <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        <div className="flex flex-col lg:flex-row lg:space-x-8">
-          <p className="max-w-xl text-lg">
-            I&apos;m a web developer with a passion for creating beautiful and functional user experiences.
-            With a background in [Your Background], I specialize in [Your Specialization].
-          </p>
+        <div className="flex flex-col lg:space-x-8 lg:space-y-4">
+          <div className="text-3xl font-bold">
+            Hi! I'm Philip Roddenberry
+          </div>
+          <div className="max-w-xl text-lg">
+          A passionate web developer with a knack for creating dynamic and user-friendly websites. With a strong foundation in both front-end and back-end technologies, I thrive on transforming complex problems into simple, elegant solutions. My goal is to build digital experiences that not only look great but also perform seamlessly. When I'm not coding, you can find me exploring the latest tech trends or contributing to open-source projects.
+          </div>
         </div>
       </section>
     )
