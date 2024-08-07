90fbf3ad2039f58cac8642237532911fdc50d245
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-06 19:55:12 -0600

    fix width layout for About

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,9 +1,9 @@
 export default function About() {
     return (
       <section id="about" className="p-8">
-        <h2 className="flex text-3xl font-bold mb-4 pr-10 justify-end w-full">About Me</h2>
-        <div className="flex flex-col">
-          <p className="text-lg">
+        <h2 className="text-3xl font-bold mb-4">About Me</h2>
+        <div className="flex flex-col lg:flex-row lg:space-x-8">
+          <p className="max-w-xl text-lg">
             I&apos;m a web developer with a passion for creating beautiful and functional user experiences.
             With a background in [Your Background], I specialize in [Your Specialization].
           </p>
