75441681ad010d78c93be0da1cbcdb1046358d74
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-08 01:26:03 -0600

    reword content for first project in Project

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -6,7 +6,7 @@ export default function Projects() {
           <div className="pl-4 space-y-1">
             <h3 className="text-2xl font-bold">PF2e AIOQR</h3>
             <p className="text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
-            <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup</p>
+            <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup process to allow a Game Master to run the table as smoothly as possible without the need for encyclopedic game knowledge or breaks in the action while they look up rules. </p>
           </div>
           <div className="pl-4 space-y-1">
             <h3 className="text-2xl font-bold">PF2e AIOQR</h3>
