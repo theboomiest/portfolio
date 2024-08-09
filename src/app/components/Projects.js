6fa0b474153e985d7fb3629a0eeed6535b088a32
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-09 16:18:43 -0600

    add umbrage.world to Projects and fix subheader max width

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -5,17 +5,17 @@ export default function Projects() {
         <div className="space-y-4">
           <div className="pl-4 space-y-1">
             <h3 className="text-2xl font-bold">PF2e AIOQR</h3>
-            <p className="text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
+            <p className="max-w-xl text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
             <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup process to allow a Game Master to run the table as smoothly as possible without the need for encyclopedic game knowledge or breaks in the action while they look up rules. </p>
           </div>
           <div className="pl-4 space-y-1">
-            <h3 className="text-2xl font-bold">PF2e AIOQR</h3>
-            <p className="text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
-            <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup</p>
+            <h3 className="text-2xl font-bold">umbrage.world</h3>
+            <p className="max-w-xl text-xl text-slate-600">Campaign website for Pathfinder 2e with custom wiki-style setting documentation</p>
+            <p className="max-w-xl text-lg text-slate-900">A Wordpress website for a custom PF2e campaign. Featuring a robust wiki that contains all the custom setting information compiled using Quartz interpreting a directory of Markdown files. The setting page is hosted on a static GitPage.</p>
           </div>
           <div className="pl-4 space-y-1">
             <h3 className="text-2xl font-bold">PF2e AIOQR</h3>
-            <p className="text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
+            <p className="max-w-xl text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
             <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup</p>
           </div>
           {/* Add more experiences as needed */}
