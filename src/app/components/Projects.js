3f865d4081095c60775a374fb33b4f9d2c612cf2
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-09 22:51:18 -0600

    add icon for umbrage.world in Projects

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Projects() {
     return (
       <section id="projects" className="p-8">
@@ -9,8 +11,11 @@ export default function Projects() {
             <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup process to allow a Game Master to run the table as smoothly as possible without the need for encyclopedic game knowledge or breaks in the action while they look up rules. </p>
           </div>
           <div className="pl-4 space-y-1">
-            <h3 className="text-2xl font-bold"><a href="umbrage.world">umbrage.world</a></h3>
-            <p className="max-w-xl text-xl text-slate-600">Campaign website for Pathfinder 2e with custom wiki-style setting documentation</p>
+            <div className="flex items-start space-x-2">
+              <a href='umbrage.world'><Image src="/burning-skull-icon.png" alt="Burning skull icon for umbrage.world" width={32} height={32}/></a>
+              <h3 className="text-2xl font-bold"><a href="umbrage.world">umbrage.world</a></h3>
+            </div>
+            <p className="pl-4 max-w-xl text-xl text-slate-600">Campaign website for Pathfinder 2e with custom wiki-style setting documentation</p>
             <p className="max-w-xl text-lg text-slate-900">A Wordpress website for a custom PF2e campaign. Featuring a robust wiki that contains all the custom setting information compiled using Quartz interpreting a directory of Markdown files. The setting page is hosted on a static GitPage.</p>
           </div>
           <div className="pl-4 space-y-1">
@@ -18,7 +23,7 @@ export default function Projects() {
             <p className="max-w-xl text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
             <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup</p>
           </div>
-          {/* Add more experiences as needed */}
+          {/* Add more projects as needed */}
         </div>
       </section>
     )
