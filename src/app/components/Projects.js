6c1f08dca6b87f8b4fd6210b141051095eb8bb3a
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-11 00:19:44 -0600

    remove last placeholder from Projects

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -18,11 +18,14 @@ export default function Projects() {
             <p className="pl-4 max-w-xl text-xl text-slate-600">Campaign website for Pathfinder 2e with custom wiki-style setting documentation</p>
             <p className="max-w-xl text-lg text-slate-900">A Wordpress website for a custom PF2e campaign. Featuring a robust wiki that contains all the custom setting information compiled using Quartz interpreting a directory of Markdown files. The setting page is hosted on a static GitPage.</p>
           </div>
-          <div className="pl-4 space-y-1">
-            <h3 className="text-2xl font-bold">PF2e AIOQR</h3>
-            <p className="max-w-xl text-xl text-slate-600">Pathfinder 2nd Edition All In One Quick Reference</p>
-            <p className="max-w-xl text-lg text-slate-900">A webapp Game Master screen for the second edition of the table top role playing game Pathfinder. As a traditional pen and paper style TTRPG, Pathfinder 2e has an enormous catalogue of rules that must either be memorized or readily available to play the game. This quick reference is an attempt by me to streamline the rules lookup</p>
-          </div>
+          {/*<div className="pl-4 space-y-1">
+            <div className="flex items-start space-x-2">
+              <a href='https://umbrage.world' target='_blank'><Image src="/burning-skull-icon.png" alt="Burning skull icon for umbrage.world" width={32} height={32}/></a>
+              <h3 className="text-2xl font-bold"><a href="https://umbrage.world" target='_blank'>umbrage.world</a></h3>
+            </div>
+            <p className="pl-4 max-w-xl text-xl text-slate-600">Campaign website for Pathfinder 2e with custom wiki-style setting documentation</p>
+            <p className="max-w-xl text-lg text-slate-900">A Wordpress website for a custom PF2e campaign. Featuring a robust wiki that contains all the custom setting information compiled using Quartz interpreting a directory of Markdown files. The setting page is hosted on a static GitPage.</p>
+          </div>*/}
           {/* Add more projects as needed */}
         </div>
       </section>
