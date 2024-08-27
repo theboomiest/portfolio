c0d5d39fad1e385a9debc89de43b1ae1c21e5a65
Philip Roddenberry <pmrodden4@gmail.com>
2024-08-26 23:32:37 -0600

    adjust hover card bg color for About section

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
           <div className="text-5xl font-bold">
             Hi! I&apos;m Philip Roddenberry
           </div>
-          <div className="text-lg p-4 space-y-2 bg-transparent transition-all duration-300 ease-in-out hover:bg-yellow-700 hover:bg-opacity-20 rounded-lg hover:shadow-lg">
+          <div className="text-lg p-4 space-y-2 bg-transparent transition-all duration-300 ease-in-out hover:bg-yellow-600 hover:bg-opacity-20 rounded-lg hover:shadow-lg">
           A passionate web developer with a knack for creating dynamic and user-friendly websites. With a strong foundation in both front-end and back-end technologies, I thrive on transforming complex problems into simple, elegant solutions. My goal is to build digital experiences that not only look great but also perform seamlessly. When I&apos;m not coding, you can find me hunched over a chessboard or getting lost in the mountains.
           </div>
         </div>
